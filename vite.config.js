d61c18386ba4c2d101bb865f02a07f79a18fb50c
heysa3id <said.elhamaoui@um6p.ma>
2026-03-13 01:12:57 +0000

    V3

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', // use '/' for custom domain (e.g. www.pvcopilot.com)
   plugins: [react()],
 })
